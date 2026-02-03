e8f5b3f88d29e56667c1aba68859ea1ebd881c98
FrsErley <frsleeh@gmail.com>
2026-02-03 12:04:48 -0300

    fix: ensure url is correct

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:3000/api" });
+export const api = axios.create({ baseURL: "https://todolist-api-ylqi.onrender.com" });
 
 api.interceptors.request.use(
   (config) => {
